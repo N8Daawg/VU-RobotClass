44adc8c53470b1cf4611b06c788ac31bcf7330be
N8Daawg <nbeals50@gmail.com>
2024-02-28 15:57:04 -0600

    updates to drivetrain

--- src/DriveTrain.cpp
+++ src/DriveTrain.cpp
@@ -6,3 +6,26 @@
 /*    Description:  file for storing DriveTrian class code                    */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+class drivetrain {
+    
+    private:
+        double wheelRadius;
+        double gearRatio;
+        motor Fl;
+        motor Fr;
+    
+    public:
+        drivetrain(motor fl, motor fr, double wr, double gr){
+            Fl=motor(fl);
+            Fr=motor(fr);
+            wheelRadius=wr;
+            gearRatio=gr;    
+        }
+
+        double getGearRatio(){return gearRatio;}
+        double getWheelRadius(){return wheelRadius;}
+
+
+};
